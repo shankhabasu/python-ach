2ff2ca96f146c3306d9afbd39736462b845a1e63
Jared Hobbs <jared@pyhacker.com>
2018-03-16 09:25:13 -0700

    bump version/name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='ach',
+    name='carta-ach',
     author='Travis Hathaway',
     author_email='travis.j.hathaway@gmail.com',
-    version='0.2.1',
+    version='0.3.0',
     packages=[
         'ach',
     ],
-    url='https://github.com/travishathaway/python-ach',
+    url='https://github.com/carta/python-ach',
     license='MIT License',
     description='Library to create and parse ACH files (NACHA)',
     long_description=open('README.rst').read(),
